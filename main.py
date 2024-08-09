16d7bfebc4ca86532f9a321fea41399b257f15d1
keenai-io <vince@keenai.io>
2024-08-09 18:25:35 -0400

    latest

--- main.py
+++ main.py
@@ -9,10 +9,8 @@ from components._partials.footer import add_footer
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
 
-# Set layout to 'wide' for better mobile responsiveness
-st.set_page_config(page_title="KeenAI", page_icon="./static/img/favicon.ico", layout="wide")
+st.set_page_config(page_title="KeenAI", page_icon="./static/img/favicon.ico", layout="centered")
 
-# Ensure the navbar is responsive (this part may require custom CSS)
 current_page = get_current_page_from_navbar()
 
 if current_page == "Home":
@@ -25,43 +23,3 @@ elif current_page == "Contact Us":
     contact_page()
 
 add_footer()
-
-# Optionally, you can add custom CSS for better mobile support
-st.markdown(
-    """
-    <style>
-    /* Responsive text scaling */
-    body, h1, h2, h3, h4, h5, h6 {
-        font-size: calc(12px + 0.5vw);
-    }
-
-    /* Navbar responsiveness */
-    .navbar {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-        align-items: center;
-    }
-
-    @media only screen and (max-width: 600px) {
-        .navbar {
-            flex-direction: column;
-        }
-
-        .navbar-item {
-            font-size: 14px;
-        }
-    }
-
-    /* Ensure all elements scale with screen size */
-    .container, .main {
-        max-width: 100%;
-        padding: 0 5%;
-    }
-
-    .footer {
-        margin-top: 2rem;
-    }
-    </style>
-    """, unsafe_allow_html=True
-)
